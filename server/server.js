5613f17f40b090376534536e1c82f991fb5e598b
kimberlywycheung <kimberlywycheung@gmail.com>
2023-01-07 20:53:38 -0800

    server changes

--- server/server.js
+++ server/server.js
@@ -1,11 +1,8 @@
-const cluster = require('node:cluster');
-const http = require('node:http');
-const { cpus } = require('node:os');
-const process = require('node:process');
-
+const cluster = require('cluster');
+const http = require('http');
+const numCPUs = require("os").cpus().length;
+const process = require('process');
 const app = require('./index.js');
-
-const numCPUs = cpus().length;
 
 if (cluster.isPrimary) {
   console.log(`Primary ${process.pid} is running`);
@@ -16,6 +13,7 @@ if (cluster.isPrimary) {
 
   cluster.on('exit', (worker, code, signal) => {
     console.log(`worker ${worker.process.pid} died`);
+    cluster.fork();
   });
 } else {
   app.listen(8000, console.log(`Worker ${process.pid} running on http://localhost:8000`));
